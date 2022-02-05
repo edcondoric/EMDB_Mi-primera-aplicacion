923c267d876bbdf9a2bb0a1ecca4bd59fa0524bf
edcondoric <edcondoriccora@gmail.com>
2022-02-05 00:12:25 -0500

    Pares E Impares

--- app.js
+++ app.js
@@ -6,6 +6,24 @@ const capitalize = (s) => {
   return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
+
+app.get('/',(req, res) => {
+	
+	let valor = "";
+	for (let i = 1; i <= 50; i++ ){
+		if(i%2 == 0) valor += '<p>'+i+' Soy Par! </p>';
+		else valor += '<p>'+i+' Soy Impar! </p>';	
+	}
+	res.send(valor);
+});
+
+
+
+
+/*
+
+Reto 1
+
 app.get('/makers/:nombre', (req, res) => {
   // let nombre = req.query.name;
   let { nombre } = req.params;
@@ -17,5 +35,7 @@ app.get('/makers/:nombre', (req, res) => {
 });
 
 // express deprecated req.param(name): Use req.params, req.body, or req.query instead app.js:6:20
+
+*/
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
