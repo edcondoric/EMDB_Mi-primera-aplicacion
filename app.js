7f3f3f74f05854165c2c94772a77c1579a63a783
edcondoric <edcondoriccora@gmail.com>
2022-02-04 23:35:49 -0500

    Saludame 1

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>HcUy6Re2LLBRtj</h1>');
+  // let nombre = req.query.name;
+  let nombre = req.param('name');
+  if(nombre == null || nombre == ''){
+      nombre = "desconocido";
+  }
+  res.send('<h1>Hola ' + nombre + '!</h1>');
 });
+
+// express deprecated req.param(name): Use req.params, req.body, or req.query instead app.js:6:20
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
