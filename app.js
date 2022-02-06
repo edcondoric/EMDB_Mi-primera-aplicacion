f73d5bc94f9e231b3906e691b7b5cd9302d9df8a
edcondoric <edcondoriccora@gmail.com>
2022-02-06 15:49:29 -0500

    Visitantes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 const capitalize = (s) => {
   if (typeof s !== 'string') return ''
